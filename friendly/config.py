f0e157a3dacbb6fbd61166be3a3fd6f6fed08d26
Bogdan <bogdanatrosenko@gmail.com>
2024-07-30 20:54:37 +0300

    Store sensitive data into .env | create table user | create sync_engine for postgresql | Insert data by sql and orm

---
+++ friendly/config.py
@@ -0,0 +1,22 @@
+import os
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+DOTENV = os.path.join(os.path.dirname(__file__), "../.env")
+
+
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+    @property
+    def db_url_psycopg(self):
+        return f'postgresql+psycopg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
+
+    model_config = SettingsConfigDict(env_file=DOTENV)
+
+
+settings = Settings()
