6f234b753c2dc194bb6566de52f9514859c683eb
Bogdan <bogdanatrosenko@gmail.com>
2024-09-18 13:50:41 +0300

    Reorganize html templates (bring out common points in index.html ) | Template generation via jinja2

--- friendly/mail/mail_sender.py
+++ friendly/mail/mail_sender.py
@@ -3,6 +3,7 @@ from typing import Dict
 
 from config import settings
 from fastapi_mail import ConnectionConfig, FastMail, MessageSchema, MessageType
+from jinja2 import Environment, FileSystemLoader
 from pydantic import EmailStr
 from starlette.responses import JSONResponse
 
@@ -22,7 +23,17 @@ conf = ConnectionConfig(
 async def send_mail(email: EmailStr, body: Dict, sub: str, temp_name: str) -> JSONResponse:
     """Отправить письмо на почту"""
     body["support_email"] = settings.MAIL_USERNAME
-    message = MessageSchema(subject=sub, recipients=[email], template_body=body, subtype=MessageType.html)
+    template = get_html_template(body, temp_name)
+    message = MessageSchema(subject=sub, recipients=[email], body=template, subtype=MessageType.html)
     fm = FastMail(conf)
-    await fm.send_message(message, template_name=temp_name)
+    await fm.send_message(message)
     return JSONResponse(status_code=200, content={"message": "email has been sent"})
+
+
+def get_html_template(data: dict, template_name: str) -> str:
+    """Сгенерировать html шаблон для отправки на почту"""
+    file_loader = FileSystemLoader(Path(__file__).parent / "templates")
+    env = Environment(loader=file_loader)
+
+    template = env.get_template(template_name)
+    return template.render(data)
