2b0dd5adfd1550d4438f4bf12d18c0423787548e
Bogdan <bogdanatrosenko@gmail.com>
2025-06-12 17:03:59 +0300

    BugFix: change FCM token length from 150 to 100 characters

--- friendly/application/notifications/request_body.py
+++ friendly/application/notifications/request_body.py
@@ -7,6 +7,6 @@ class DeviceTokenFCM(BaseModel):
     device_token: str = Field(
         examples=["eI8V05b_ZX1eYfXdAs9EuE:APA91bHLObWjb_LNanOwB7D38_mbxudQCE6P..."],
         description="The device ID that was received when accessing Firebase on the client side",
-        min_length=150,
+        min_length=100,
         max_length=256,
     )
